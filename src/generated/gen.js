77f3bc85920e78e24646e31cb3f3f67ec89f329a
esadyalcin <esad@rysesoft.com>
2024-03-28 17:25:55 +0300

    sync

--- src/generated/gen.js
+++ src/generated/gen.js
@@ -2,13 +2,13 @@
 
 export const needle_exported_files = new Array();
 globalThis["needle:codegen_files"] = needle_exported_files;
-needle_exported_files.push("./assets/Pressure.glb?v=1711631656337");
+needle_exported_files.push("./assets/Pressure.glb?v=1711635824201");
 document.addEventListener("DOMContentLoaded", () =>
 {
 	const needleEngine = document.querySelector("needle-engine");
 	if(needleEngine && needleEngine.getAttribute("src") === null)
 	{
-		needleEngine.setAttribute("hash", "1711631656337");
+		needleEngine.setAttribute("hash", "1711635824201");
 		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
 	}
 });
